1e445ed71de0b22620f5b0489fc3a49223bad5de
Your Name <you@example.com>
2025-07-31 22:19:40 +0200

    first commit

---
+++ src/components/hero.tsx
@@ -0,0 +1,29 @@
+export default function Hero (){
+    return(
+       
+<div className="bg-white ">
+<div className="relative isolate px-6 pt-14 lg:px-8 flex justify-start">
+    <div aria-hidden="true" className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+    </div>
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="hidden sm:mb-8 sm:flex ">
+        <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span aria-hidden="true" className="absolute inset-0"></span>Read more <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </div>
+      <div className="">
+        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">BOUMAZIAN FADOUA</h1>
+        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Technicien specialiser en infrastructure digitale option cloud computing. And devolopement web </p>
+        <div className="mt-10 flex items-center  gap-x-6">
+          <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+        </div>
+      </div>
+    </div>
+    <div aria-hidden="true" className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+    </div>
+  </div>
+</div>
+
+    )
+}
