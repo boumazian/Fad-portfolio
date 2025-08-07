a39097273c6283acebbfd636121a57a106fd03ed
Your Name <you@example.com>
2025-08-07 12:28:07 +0200

    add link in navbar

---
+++ src/app/aboutme/page.tsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+  return (
+    <div className="max-w-4xl mx-auto px-4 sm:px-8 py-10">
+      <p className="mb-6 text-gray-600 dark:text-gray-400 text-lg leading-relaxed first-line:uppercase first-line:tracking-widest 
+      
+      first-letter:text-7xl first-letter:font-bold
+       first-letter:text-gray-900 dark:first-letter:text-white first-letter:me-4 first-letter:float-start">
+        Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other 
+        software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to 
+        requests, incidents, and changes.
+      </p>
+      <p className="text-gray-600 dark:text-gray-400 text-base leading-relaxed">
+        Deliver great service experiences fast — without the complexity of traditional ITSM solutions. Accelerate 
+        critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.
+      </p>
+    </div>
+  );
+}
